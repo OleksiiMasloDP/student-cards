f541875a053418f9103dbbd9f07033e58c359ce8
palamoovv <kostyaklimenko2011@gmail.com>
2026-04-03 17:27:43 +0400

    added few changes in creating a cards

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 let int = document.getElementById("nameInt");
 let gender = document.getElementById("genderSelect");
 const container = document.getElementById("container");
+const cardBox = document.getElementById("card-container")
 
 function randomNum() {
   return Math.floor(Math.random() * 100) + 1;
 }
 
-console.log(randomNum());
-
 function createCard() {
+  cardBox.innerHTML = "" 
   const div = document.createElement("div");
   div.classList.add("card", "w-25");
-  div.innerHTML = `<img class="card-img-top" src="https://randomuser.me/api/portraits/${gender.value}/${randomNum()}.jpg"/>\
+  div.innerHTML = `<img class="card-img-top" src="https://randomuser.me/api/portraits/${gender.value.toLowerCase()}/${randomNum()}.jpg"/>\
   <div class="card-body">
     <h5 class="card-title">${int.value}</h5>
     </div>`;
-  container.appendChild(div);
+  cardBox.appendChild(div);
   int.value = "";
 }
